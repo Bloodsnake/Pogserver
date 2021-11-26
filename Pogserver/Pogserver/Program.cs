46f3f28320f90117d8476c5b0f834c5f16077489
Bloodsnake <58860946+Bloodsnake@users.noreply.github.com>
2021-11-26 13:07:47 +0100

    Added server components
    Added server components

---
+++ Pogserver/Pogserver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Pogserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Server();
+            Task something = server.StartServer("");
+            something.GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ Pogserver/Pogserver/Server.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pogserver
+{
+    class Server
+    {
+        public bool IsRunning { get; private set; }
+        public string Url { get; private set; }
+        public HttpListener Listener { get; private set; }
+        public Server(string url = "http://localhost", uint port = 8000)
+        {
+            this.Url = url + ":" + port + "/";
+            this.Listener = new HttpListener();
+
+            this.Listener.Prefixes.Add(this.Url);
+            this.Listener.Start();
+        }
+
+        public async Task StartServer(string pageFolder)
+        {
+            this.IsRunning = true;
+            while (this.IsRunning)
+            {
+                HttpListenerContext ctx = await this.Listener.GetContextAsync();
+
+                HttpListenerRequest req = ctx.Request;
+                HttpListenerResponse resp = ctx.Response;
+
+                if (req.HttpMethod == "GET" && req.Url.AbsolutePath == "/")
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(String.Format(File.ReadAllText(pageFolder + "/index.html")));
+                    resp.ContentType = "text/html";
+                    resp.ContentEncoding = Encoding.UTF8;
+                    resp.ContentLength64 = data.LongLength;
+
+                    await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                    resp.Close();
+                }
+                else if (req.HttpMethod == "GET" && req.Url.AbsolutePath == "/shutdown")
+                {
+                    this.IsRunning = false;
+                }
+            }
+        }
+    }
+}
