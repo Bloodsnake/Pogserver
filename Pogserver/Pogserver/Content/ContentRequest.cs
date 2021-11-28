cecdcc43320878f2dd3c67588d78aa76788c9845
Bloodsnake <58860946+Bloodsnake@users.noreply.github.com>
2021-11-28 17:31:29 +0100

    Moved HTML resources

--- Pogserver/Pogserver/Content/ContentRequest.cs
+++ Pogserver/Pogserver/Content/ContentRequest.cs
@@ -2,9 +2,10 @@
 {
     class ContentRequest : IRequest
     {
-        public ContentRequest(string contentPath)
+        public ContentRequest(string contentPath, IRequest.HTTPType type)
         {
             this.ContentPath = contentPath;
+            this.Type = IRequest.RequestType.Content;
         }
         public string ContentPath { get; set; }
         public IRequest.RequestType Type { get; set; }
