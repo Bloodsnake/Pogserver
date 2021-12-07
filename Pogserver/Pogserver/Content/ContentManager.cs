04a414744e803c6822acdb710262e5b3ec294b97
Bloodsnake <58860946+Bloodsnake@users.noreply.github.com>
2021-12-07 20:02:24 +0100

    Added ContentManager

---
+++ Pogserver/Pogserver/Content/ContentManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Pogserver.Content
+{
+    class ContentManager
+    {
+        public static readonly string Path = "/Content";
+        public static Dictionary<string, IRequest> GetAllContentRequests()
+        {
+            var requests = new Dictionary<string, IRequest>();
+
+            requests.Add("/", new ContentRequest("index.html"));
+            requests.Add("/shutdown", new ContentRequest("shutdown.html"));
+
+            var common = GetAllCommonFiles();
+            common.ToList().ForEach(x => requests.Add(x.Key, x.Value));
+
+            return requests;
+        }
+        public static Dictionary<string, IRequest> GetAllCommonFiles()
+        {
+            var requests = new Dictionary<string, IRequest>();
+
+            foreach (var file in Directory.GetFiles(Environment.CurrentDirectory + Path + "/Common/"))
+            {
+                var c = @"\".ToCharArray()[0];
+                var path = file.Replace(Environment.CurrentDirectory + Path, "").Replace(c, '/');
+                requests.Add(path, new ContentRequest(path));
+            }
+
+            return requests;
+        }
+    }
+}
